6feb877a7077c6abe7576019a517c91ce766786e
cody-jarrett <cody-jarrett@users.noreply.github.com>
2022-05-12 22:06:17 -0700

    add lock, update ver

--- blueprint/__init__.py
+++ blueprint/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 # Add ROOT_DIR variable, containing the absolute path to the location of the package?
 # Add the configuration of the logger for the package?
