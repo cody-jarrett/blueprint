8eaa5d4747878fb32d9b6662f307162432e348b6
cody-jarrett <cody-jarrett@users.noreply.github.com>
2022-05-12 22:42:15 -0700

    simplify ver handling

--- blueprint/__init__.py
+++ blueprint/__init__.py
@@ -1,4 +1,6 @@
-__version__ = '0.2.0'
+import toml
+
+__version__ = toml.load("pyproject.toml")["tool"]["poetry"]["version"]
 
 # Add ROOT_DIR variable, containing the absolute path to the location of the package?
 # Add the configuration of the logger for the package?
--- blueprint/main.py
+++ blueprint/main.py
@@ -3,11 +3,13 @@ import os
 import toml
 from dotenv import load_dotenv
 
+from blueprint import __version__
+
 CONFIG = toml.load("config.toml")["app"]
 
 load_dotenv(".env")
 
 
 def app():
-    print(f"This is {CONFIG.get('APP_NAME')}")
+    print(f"This is {CONFIG.get('APP_NAME')} {__version__}")
     print(f"Here's a test secret: {os.environ.get('TEST')}")
