53f3ec3552579efe234bcd9a5944dc8bf6f35767
cody-jarrett <cody-jarrett@users.noreply.github.com>
2022-05-12 22:25:17 -0700

    remove secrets module

--- blueprint/main.py
+++ blueprint/main.py
@@ -1,14 +1,14 @@
+import os
+
 import toml
 from dotenv import load_dotenv
 
-from .secret import print_secret
-
-APP_CONFIG = toml.load("config.toml")["app"]
+CONFIG = toml.load("config.toml")["app"]
 
 load_dotenv(".env")
 
 
 def app():
     """This is the main entrypoint to your application"""
-    print(f"This is {APP_CONFIG.get('APP_NAME')}")
-    print_secret()
+    print(f"This is {CONFIG.get('APP_NAME')}")
+    print(f"Here's a test secret: {os.environ.get('TEST')}")
