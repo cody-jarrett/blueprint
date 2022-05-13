4ea170ee1fceb8bed82367866fb71d9cd811f075
cody-jarrett <cody-jarrett@users.noreply.github.com>
2022-05-12 22:23:30 -0700

    set up dotenv properly

--- blueprint/main.py
+++ blueprint/main.py
@@ -5,10 +5,10 @@ from .secret import print_secret
 
 APP_CONFIG = toml.load("config.toml")["app"]
 
-load_dotenv()
+load_dotenv(".env")
 
 
 def app():
     """This is the main entrypoint to your application"""
-    print(f"This is {APP_CONFIG['APP_NAME']}. Let's build some cool python apps!")
+    print(f"This is {APP_CONFIG.get('APP_NAME')}")
     print_secret()
--- blueprint/secret.py
+++ blueprint/secret.py
@@ -2,4 +2,4 @@ import os
 
 
 def print_secret():
-    print(f"Let's keep it secret:{os.environ['APP_SECRET']}")
+    print(f"Here's a test secret: {os.environ.get('TEST')}")
